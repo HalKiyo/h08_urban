a398dde8d8efea78e636b9f5b09eedcbc43734dd
HalKiyo <hasegawa.k.ai@m.titech.ac.jp>
2024-09-20 15:39:41 +0900

    nomask done

--- qgis/pre/h082qgis.py
+++ qgis/pre/h082qgis.py
@@ -3,21 +3,17 @@ import rasterio
 from rasterio.transform import from_origin
  
 def bin2las(loadpath, savepath):
-    # バイナリデータの読み込みと変換
     data = np.fromfile(loadpath, dtype='float32')
-    data = data.reshape([2160, 4320]) # データの行数と列数
+    data = data.reshape([2160, 4320])
 
-    # 10000以下の値をnodataに置き換える
     #data[data <= 10000] = 0
 
-    # ラスタデータの変換パラメータを設定
-    west = -180 # 西経
-    north = 90 # 北緯
+    west = -180
+    north = 90
     transform = from_origin(west, north, 5/60, 5/60)
 
-    # 新しいGeoTIFFファイルを作成
     with rasterio.open(savepath, 'w', driver='GTiff', height=data.shape[0], width=data.shape[1], count=1, dtype=str(data.dtype), crs='+proj=latlong', transform=transform) as dst:
-       dst.nodata = 0 # nodata値を0に設定
+       dst.nodata = 0 
        dst.write(data, 1)
     print(f"{savepath} is saved")
 
@@ -26,8 +22,8 @@ def main():
     loadpath = '/mnt/c/Users/tsimk/Downloads/dotfiles/h08/global_city/dat/cty_swg_/gpw3/city_00000000.gl5'
     savepath = '/mnt/c/Users/tsimk/Downloads/dotfiles/h08/qgis/dat/cty_msk_gpw3_00000000.tif'
     """
-    loadpath = '/mnt/c/Users/tsimk/Downloads/dotfiles/h08/global_city/dat/vld_cty_/city_clrdovlp.gl5'
-    savepath = '/mnt/c/Users/tsimk/Downloads/dotfiles/h08/qgis/dat/vld_city_city_clrdovlp.tif'
+    loadpath = '/mnt/c/Users/tsimk/Downloads/dotfiles/h08/camacity/dat/cty_msk_/city_clrd0000.gl5'
+    savepath = '/mnt/c/Users/tsimk/Downloads/dotfiles/h08/qgis/dat/camacity_clrd0000.tif'
     bin2las(loadpath, savepath)
 
 if __name__ == '__main__':
